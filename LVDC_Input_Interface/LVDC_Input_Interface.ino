6f95a96b80b30bea2baf5d7d38fffdaf9291134c
Brandon Lantau <113632274+discount-cactus@users.noreply.github.com>
2023-02-28 21:28:54 -0700

    Updated library code

--- LVDC_Input_Interface/LVDC_Input_Interface.ino
+++ LVDC_Input_Interface/LVDC_Input_Interface.ino
@@ -3,6 +3,11 @@
 
 #include "LVDC.h"
 
+#define button1Pin 4
+#define button2Pin 2
+
+#define clockPin 9
+
 const int address[28] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49};
 int instruction[28] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
@@ -13,13 +18,23 @@ LVDC lvdc;
 void setup(){
   lvdc.setAddress(address); //Sets the pins as outputs
   lvdc.writeToAddress(instruction); //Writes the output to the bus
+
+  pinMode(clockPin,OUTPUT);
 }
 
 void loop(){
   //test_random_pins(); delay(1000);
-  test_sweep();
-  //clock_pulse_2_048_MHZ()
+  //test_sweep();
+  //clock_pulse_2_048_MHZ();
+  digitalWrite(clockPin,HIGH);
+  delay(500);
+  digitalWrite(clockPin,LOW);
+  delay(500);
 }
+
+
+
+
 
 void test_random_pins(){
   for(int i=0; i < 28; i++){
